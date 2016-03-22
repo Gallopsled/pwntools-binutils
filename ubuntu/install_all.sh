2f310a7785d12f5069baa07917cf6cfb1bb6086d
C0deH4cker <c0deh4cker@gmail.com>
2016-03-22 19:39:55 -0400

    Make install_all.sh not require an unused argument

--- ubuntu/install_all.sh
+++ ubuntu/install_all.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 source ../common/arch.sh
 
-if [[ $# -ne 1 ]];
-then
-echo "usage: $0 architecture"
-else
 sudo apt-get install --yes software-properties-common
 sudo apt-add-repository --yes ppa:pwntools/binutils
 sudo apt-get update
@@ -12,4 +8,3 @@ sudo apt-get update
 for arch in $ARCHES; do
 sudo apt-get install binutils-$arch-linux-gnu
 done
-fi
